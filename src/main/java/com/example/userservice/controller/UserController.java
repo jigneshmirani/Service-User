883247e79df760e68ff6b508dede345220a3bf50
jignesh.mirani <jignesh.mirani@host.docker.internal>
2021-03-11 17:38:03 +0530

    Version 1.15

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
 	public String getSampleData()
 	{
 		log.info("Sample data method");
-		return "Welcome to jenkins world -- version 1.13";
+		return "Welcome to jenkins world -- version 1.14";
 	}
 	
 	@PostMapping("/saveData")
