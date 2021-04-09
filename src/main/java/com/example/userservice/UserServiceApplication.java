f0d41c3f2af6c7abc13f6d4bea2dc3dfbd868243
jignesh.mirani <jignesh.mirani@CWLT060.corp.capitaworld.com>
2021-04-09 17:43:28 +0530

    version 1.41 and also added swagger changes.

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -5,8 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -17,6 +24,16 @@ public class UserServiceApplication {
 	public RestTemplate restTemplate()
 	{
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public Docket swaggerConfiguration()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/Users/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.example"))
+				.build();
 	}
 
 }
